157a172e23b8aafcc40a1d77da996e7346edcc23
Florian Schmidt <Florian.Schmidt@dlr.de>
2014-09-23 14:49:31 +0000

    reworked get_ln_base.py to return LN_BASE env var when there is not pkgtool

--- scripts/get_ln_base.py
+++ scripts/get_ln_base.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python
 
+import os
 import sys
 import subprocess
+
+pkgtool = "/volume/software/common/packages/rmpm/latest/bin/sled11-x86-gcc4.x/pkgtool"
 
 for arg in sys.argv[1:]:
     entries = arg.split(',')
 
     for entry in entries:
         if entry.strip().startswith("software.links_and_nodes"):
-            p = subprocess.Popen(["/volume/software/common/packages/rmpm/latest/bin/sled11-x86-gcc4.x/pkgtool", "--key", "PKGROOT", entry.strip()])
-            p.wait()
+            if os.path.isfile(pkgtool):
+                p = subprocess.Popen([pkgtool, "--key", "PKGROOT", entry.strip()])
+                p.wait()
+            else:
+                print os.getenv("LN_BASE")
 
