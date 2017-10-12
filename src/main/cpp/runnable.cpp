73bc41e78823a34b422fb1bcb6e8943ed6515281
Robert Burger <robert.burger@dlr.de>
2017-10-12 07:38:41 +0200

    set thread name also if thread was not startet untill now

--- src/main/cpp/runnable.cpp
+++ src/main/cpp/runnable.cpp
@@ -146,7 +146,8 @@ void runnable::set_affinity_mask(int mask) {
 void runnable::set_name(std::string name) {
 #ifdef HAVE_PTHREAD_SETNAME_NP
     klog(verbose, "[runnable] setting thread name to %s\n", name.c_str());
-	pthread_setname_np(tid, name.c_str());
+    thread_name = name;
+    pthread_setname_np(tid, name.c_str());
 #endif
 }
 
