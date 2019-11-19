23e0b066029a87bc621fcdc9e73c033d5ed7294f
Robert Burger <robert.burger@dlr.de>
2019-11-19 09:40:19 +0100

    now using conan_template python_requires

--- conanfile.py
+++ conanfile.py
@@ -1,14 +1,11 @@
-from conans import ConanFile, AutoToolsBuildEnvironment, tools
-import re
+from conans import tools, python_requires
 
-class MainProject(ConanFile):
+base = python_requires("conan_template/[~=5]@robotkernel/stable")
+
+class MainProject(base.RobotkernelConanFile):
     name = "robotkernel"
-    license = "GPLv3"
-    url = f"https://rmc-github.robotic.dlr.de/robotkernel/{name}"
     description = "robotkernel-5 is a modular, easy configurable hardware abstraction framework"
-    settings = "os", "compiler", "build_type", "arch"
     exports_sources = ["*", "!.gitignore"] + ["!%s" % x for x in tools.Git().excluded_files()]
-    generators = "pkg_config"
 
     def requirements(self):
         self.requires("libstring_util/1.1.7@common/stable")
@@ -17,32 +14,3 @@ class MainProject(ConanFile):
     def config_options(self):
         self.options["libstring_util"].shared = True
 
-    def source(self):
-        filedata = None
-        filename = "project.properties"
-        with open(filename, 'r') as f:
-            filedata = f.read()
-        with open(filename, 'w') as f:
-            f.write(re.sub("VERSION *=.*[^\n]", f"VERSION = {self.version}", filedata))
-
-    def build(self):
-        self.run("autoreconf -if")
-        autotools = AutoToolsBuildEnvironment(self)
-        autotools.libs=[]
-        autotools.include_paths=[]
-        autotools.library_paths=[]
-        if self.settings.build_type == "Debug":
-            autotools.flags = ["-O0", "-g", "-fno-builtin-strlen"]
-        else:
-            autotools.flags = ["-O3"]
-        autotools.configure(configure_dir=".")
-        autotools.make()
-
-    def package(self):
-        autotools = AutoToolsBuildEnvironment(self)
-        autotools.install()
-
-    def package_info(self):
-        self.cpp_info.includedirs = ['include']
-        self.cpp_info.bindirs = ['bin']
-        self.cpp_info.resdirs = ['share']
