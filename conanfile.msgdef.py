1ea051ec58ef7225baf7908630bea2e61bba1b2f
Robert Burger <robert.burger@dlr.de>
2022-01-27 08:17:20 +0100

    CHANGE: ln msg def generation

--- conanfile.msgdef.py
+++ conanfile.msgdef.py
@@ -9,29 +9,28 @@ class MainProject(ConanFile):
     license = "GPLv3"
     description = "robotkernel ln message definitions"
     settings = None
-    exports_sources = [
-        "share/ln/message_definitions/*",
-    ]
+    generators = "pkg_config"
+    exports_sources = ["*", "!.gitignore"] + ["!%s" % x for x in tools.Git().excluded_files()]
 
     def build_requirements(self):
         self.build_requires(f"ln_helper/[~=0]@robotkernel/stable")
+        self.build_requires("libstring_util/[~=1]@common/stable")
+        self.build_requires("yaml-cpp/0.6.2@3rdparty/stable")
 
-    def requirements(self):
-        self.requires(f"robotkernel/{self.version}@{self.user}/{self.channel}")
-
-    def package(self):
+    def build(self):
+        self.run("autoreconf -if")
+        autotools = AutoToolsBuildEnvironment(self)
+        autotools.configure(configure_dir=".")
+        autotools.make()
+        
         re = RunEnvironment(self)
-        libdirs = self.deps_cpp_info["robotkernel"].lib_paths
         with tools.environment_append(re.vars):
             msgdef_dir = os.path.join(self.build_folder, "message_definitions/robotkernel")
+            filename = os.path.join(self.build_folder, "src/main/cpp/.libs/libservice_definitions.so")
+            self.run("bridge_ln_generator --so_file %s --out %s" % (filename, msgdef_dir))
 
-            for libdir in libdirs:
-                for filename in os.listdir(libdir):
-                    if filename.endswith("libservice_definitions.so"):
-                        self.run("bridge_ln_generator --so_file %s --out %s" % (os.path.join(libdir, filename), msgdef_dir))
-
+    def package(self):
         self.copy("*", "message_definitions", "message_definitions")
-
 
     def package_info(self):
         self.env_info.LN_MESSAGE_DEFINITION_DIRS.append(os.path.join(self.package_folder, "message_definitions"))
