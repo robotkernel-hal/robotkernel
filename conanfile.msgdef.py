b2097a41c105646c2b9d5adc766ead24dc0f0dd6
Maximilian Mühlbauer <maximilian.muehlbauer@dlr.de>
2023-11-10 09:52:38 +0100

    feat: conan2-compatible msgdef dirs

--- conanfile.msgdef.py
+++ conanfile.msgdef.py
@@ -1,7 +1,8 @@
 import os
 
-from conan import ConanFile
+from conan import ConanFile, conan_version
 from conan.tools.files import chdir, copy
+from conan.tools.scm import Version
 
 
 class MainProject(ConanFile):
@@ -32,4 +33,8 @@ class MainProject(ConanFile):
         copy(self, "share/ln/message_definitions/*", self.build_folder, self.package_folder)
 
     def package_info(self):
-        self.env_info.LN_MESSAGE_DEFINITION_DIRS.append(os.path.join(self.package_folder, "share/ln/message_definitions"))
+        msg_def_dir = os.path.join(self.package_folder, "share/ln/message_definitions")
+        if Version(conan_version) < "2.0.0":
+            self.env_info.LN_MESSAGE_DEFINITION_DIRS.append(msg_def_dir)
+        self.runenv_info.append_path("LN_MESSAGE_DEFINITION_DIRS", msg_def_dir)
+        self.buildenv_info.append_path("LN_MESSAGE_DEFINITION_DIRS", msg_def_dir)
