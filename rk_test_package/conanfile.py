c58766211fb9d0d7d82adf875f6bb344f338f88f
Maximilian Mühlbauer <maximilian.muehlbauer@dlr.de>
2023-11-10 09:17:14 +0100

    fix(rk test): add conanrun env

--- rk_test_package/conanfile.py
+++ rk_test_package/conanfile.py
@@ -6,8 +6,10 @@ from conan.tools.build import can_run
 class TestTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
 
+    generators = "VirtualRunEnv"
+
     def test(self):
         if can_run(self):
-            self.run("robotkernel")
+            self.run("robotkernel", env="conanrun")
         else:
             self.output.warn("Skipping run cross built package")
