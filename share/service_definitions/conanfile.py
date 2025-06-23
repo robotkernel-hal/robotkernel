d67a2d31a628a4883c03b9d8d9fd21c447f33861
Robert Burger <robert.burger@dlr.de>
2025-06-23 09:09:29 +0200

    change: moved msgdef conanfile

--- share/service_definitions/conanfile.py
+++ share/service_definitions/conanfile.py
@@ -12,22 +12,21 @@ class MainProject(ConanFile):
     license = "GPLv3"
     description = "robotkernel ln message definitions"
     settings = None
-    exports_sources = ["share/*"]
+    exports_sources = ["*"]
 
     tool_requires = ["robotkernel_ln_helper/[*]@robotkernel/stable"]
 
     generators = "VirtualBuildEnv"
 
     def build(self):
-        svc_def_dir = "share/service_definitions"
         ln_msg_dir = "share/ln/message_definitions"
 
         svc_def_files = []
-        with chdir(self, os.path.join(self.build_folder, svc_def_dir)):
-            for dirpath, _, filenames in os.walk("."):
+        with chdir(self, self.build_folder):
+            for dirpath, _, filenames in os.walk("./robotkernel"):
                 svc_def_files.extend(os.path.join(dirpath, filename) for filename in filenames)
 
-        self.run(f"service_generate --indir {svc_def_dir} --outdir {ln_msg_dir} {' '.join(svc_def_files)}", env="conanbuild")
+        self.run(f"service_generate --indir . --outdir {ln_msg_dir} {' '.join(svc_def_files)}", env="conanbuild")
 
     def package(self):
         copy(self, "share/ln/message_definitions/*", self.build_folder, self.package_folder)
