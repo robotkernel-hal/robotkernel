574cff7ed7ee231bcf482dc642e3590c62eff1f0
Robert Burger <robert.burger@dlr.de>
2025-11-04 08:43:43 +0100

    fix: build_requirements in msgdef test

--- tests/msgdef_test_package/conanfile.py
+++ tests/msgdef_test_package/conanfile.py
@@ -6,7 +6,8 @@ from conan import ConanFile
 class TestTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
 
-    self.requires(self.tested_reference_str)
+    def build_requirements(self):
+        self.requires(self.tested_reference_str)
 
     def test(self):
         pass
