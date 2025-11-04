91ec53dca6cb8c6d5ee7c12fd98b58993e1265e3
Robert Burger <robert.burger@dlr.de>
2025-11-04 08:39:17 +0100

    chore: conan2 support

--- tests/msgdef_test_package/conanfile.py
+++ tests/msgdef_test_package/conanfile.py
@@ -6,5 +6,7 @@ from conan import ConanFile
 class TestTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
 
+    self.requires(self.tested_reference_str)
+
     def test(self):
         pass
