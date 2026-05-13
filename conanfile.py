e683b153e2bf0e3a98d9b6f06712041db479d144
Robert Burger <robert.burger@dlr.de>
2026-05-13 16:34:47 +0200

    change: using conan_template 6 stable, robotkernel_generator 6 stable

--- conanfile.py
+++ conanfile.py
@@ -2,13 +2,13 @@ from conan import ConanFile
 
 
 class MainProject(ConanFile):
-    python_requires = "conan_template/6.0.0@robotkernel/unstable"
+    python_requires = "conan_template/[~6]@robotkernel/stable"
     python_requires_extend = "conan_template.RobotkernelConanFile"
 
     name = "robotkernel"
     description = "robotkernel is a modular, easy configurable hardware abstraction framework"
     exports_sources = ["*", "!.gitignore"]
-    tool_requires = ["robotkernel_generator/[~6]@robotkernel/unstable"]
+    tool_requires = ["robotkernel_generator/[~6]@robotkernel/stable"]
 
     def requirements(self):
         self.requires(f"{self.name}_ln_msgdef/{self.version}@{self.user}/{self.channel}")
